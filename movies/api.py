240c3063d45a2f1ef565895006bb2b4df7b96722
lucas <lucas@mail.daa.lan>
2023-10-30 23:05:30 +0330

    Added api app

---
+++ movies/api.py
@@ -0,0 +1,9 @@
+# myapp/api.py
+from tastypie.resources import ModelResource
+from movies.models import Movie
+
+
+class MovieResource(ModelResource):
+    class Meta:
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
